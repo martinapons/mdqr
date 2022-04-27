39041730a34c2fafdc9b823a1b4c9fe13589051c
martinapons <pons.martina@gmail.com>
2022-04-27 18:16:07 +0200

    ME TIRED

--- R/first_stage.R
+++ R/first_stage.R
@@ -1,35 +1,27 @@
 # First stage function
 
-md_first_stage <- function(data1 = subsample, dep_var, U) {
-  ### if (length(time_varying_var > 0)) { # if there are no time varying variables.
-    ### form <- as.formula(paste(dep_var, paste(time_varying_var, collapse = "+"), sep = "~"))
-    mm <- model.matrix(formula, data1) # make model matrix
+md_first_stage <- function(data1 = subsample, fdep, form1, U) {
 
-
+    mm <- model.matrix(form1, data1) # make model matrix
     qr.X <- qr(mm, tol = 1e-9, LAPACK = FALSE) # remove collinear regressors
     rnkX <- qr.X$rank # (number of non-collinear columns)
     keepp <- qr.X$pivot[seq_len(rnkX)]
     mm <- mm[, keepp] # keep only non-collinear columns
-    # form <- paste(dep_var, "~" , paste(colnames(mm)[-1], collapse  = "+"))
-    # mm <- model.frame(formula(form), data1) # make model matrix
- ### } else {
-    ### form <- as.formula(paste(dep_var, "1", sep = "~"))
-    ### mm <- stats::model.matrix(formula(form), data1) # make model matrix
-###  }
+
 
   fitted <- array(NA, dim = c(dim(data1)[1], 1, length(U)))
 
-  lambda_g <- array(0, dim = c(dim(mm)[2], dim(mm)[2], length(U))) # k = number of individual level regressors
+  lambda_i <- array(0, dim = c(dim(mm)[2], dim(mm)[2], length(U))) # k = number of individual level regressors
   if (is.null(c(dim(mm)[2]))){
-    lambda_g <- array(0, c(1,1, length(U)))
+    lambda_i <- array(0, c(1,1, length(U)))
   }
   y <- model.frame(fdep, data1)[, 1]
   for (u in U) { # For all quantiles
     qreg <- rq(y ~ mm - 1, data = data1, tau = u)
     fitted[, 1, which(U == u)] <- fitted(qreg) # Save the fitted values
-    lambda_g[, , which(U == u)] <- summary.rq(qreg, se = "ker", covariance = T)$cov / dim(data1)[1]
+    lambda_i[, , which(U == u)] <- summary.rq(qreg, se = "ker", covariance = T)$cov / dim(data1)[1]
   }
-  store <- list(fitted, lambda_g, mm)
-  names(store) <- c("fitted", "lambda_g", "mm")
+  store <- list(fitted, lambda_i, mm)
+  names(store) <- c("fitted", "lambda_i", "mm")
   return(store)
 }
--- R/main.R
+++ R/main.R
@@ -3,6 +3,7 @@ library(parallel)
 library(stringr)
 library(Formula)
 library(formula.tools)
+library(swCRTdesign)
 
 quantile <- c(.1, .5)
 
@@ -43,37 +44,30 @@ mdqr <- function(formula, data, method = c("fe", "be", "reoi", "regmm", "ols", "
   ffe <- formula(formula, lhs = 0, rhs = 4)
   fz <- formula(formula, lhs = 0, rhs = 3)
 
-  y <- model.frame(fdep, data)
- # exo <- model.frame(fex, data)
+  y <- model.frame(fdep, data) # some of these are not needed
   end <- model.frame(fen, data)
   z <- model.frame(fz, data)
   fe <- model.frame(ffe, data)
+  exo <- model.frame(fex, data)
+
   # -----------------
-  ###dep_var <- names(y)
-  ###fe_var <- names(fe)
+
   endog_var <- names(end)
-  ###exo_var <- names(exo)
-  ###inst_var <- names(z)
+  exo_var <- names(exo)
 
 
   if (length(all.vars(fen)) > length(all.vars(fen)) & method != "fe" & method != "ht") stop("fewer instruments than endogenous variables. If you wish to use interval instrument select method fe or ht")
-  if (length(all.vars(fen))== 0 & length(all.vars(fen)) > 0) stop("External instrument is specified, but there is no endogeous variable")
+  if (length(all.vars(fen)) == 0 & length(all.vars(fen)) > 0) stop("External instrument is specified, but there is no endogeous variable")
   if (min(tapply(y[, 1], group[, 1], var) > 0) == 0) stop("The dependent variable must vary within groups / individuals.")
   if (length(all.vars(fen)) == 0 & method == "fe") stop("FE is used, but no endogenous variable specified.")
-  if ( method == "ols" & length(all.vars(fen)) > 0) stop("OLS is used but there are endogenous variables.")
+  if (method == "ols" & length(all.vars(fen)) > 0) stop("OLS is used but there are endogenous variables.")
 
 
   if (sum(cbind(z, group) %>% slice_rows("group") %>% dmap(var) %>% colSums() != 0) != 1) {
     stop("The instrument is not allowed to vary within groups.")
   }
   # --------------------------------------------------------------
-###  tv <- (cbind(group, exo, end) %>% slice_rows("group") %>% summarise(across(everything(), funs(n_distinct))) %>% colSums() / G != 1)[-1]
 
- ## time_varying <- data.frame(cbind(exo, end)[, tv])
- ### time_varying_var <- c(names(cbind(exo, end))[tv == T]) # I don't like this two lines. I would like to take the names from tv (but they change)
-  ## time_constant_var <- c(names(cbind(exo, end))[tv == F])
-
- ### names(time_varying) <- time_varying_var
 
   first <- data
   datalist <- NULL
@@ -88,24 +82,25 @@ mdqr <- function(formula, data, method = c("fe", "be", "reoi", "regmm", "ols", "
   }
 
   # First stage ---------------------------------------------------------------------------------
-  U <- quantiles
-  # first stage is the same for all estimator. Only time varying variables incldued
-  # first stage estimation
-
   if (is.null(cores) == 1) {
     cores <- detectCores() - 1
   }
+
+  form1 <- formula(paste0(stringr::str_sub(as.character(fdep), 1, -5) , paste0(fex) , "+ " ,stringr::str_sub(as.character(fen), 2, -1) ))
+
   cl <- makeCluster(cores) # Set the number of clusters
   RNGkind(kind = "L'Ecuyer-CMRG") # Set the seed for each cluster
-  clusterExport(cl, c("fdep", "md_first_stage", "datalist", "formula", "U"), environment()) # Functions needed
+  clusterExport(cl, c("fdep", "md_first_stage", "datalist", "form1", "U")) # Functions needed
   clusterEvalQ(cl, {
     library(quantreg)
     library(sandwich)
     library(dtplyr)
     library(pracma)
+    library(stringr)
+    library(Formula)
     library(Matrix)
   })
-  first <- clusterApply(cl, datalist, md_first_stage, fdep, formula, U)
+  first <- clusterApply(cl, datalist, md_first_stage, fdep, form1,  U)
   stopCluster(cl)
 
   # Extract Fitted values: the fitted values are a list in a list. E
@@ -124,17 +119,14 @@ mdqr <- function(formula, data, method = c("fe", "be", "reoi", "regmm", "ols", "
     fex <- "~1"
   }
   if (method == "fe") { # no second stage FE possible if you have  method = "fe"
-    # generate internal instruments
     b <- cbind(group, end) %>%
       group_by(group) %>%
       transmute(across(everything(), funs(. - mean(.)), .names = "{.col}dem"))
-
     inst_s <- paste0(paste(endog_var, collapse = "dem +"), "dem")
     second <- bind_cols(b[, -1], data, fitted)
-
     form <- as.Formula(paste0("c(", paste(mydep, collapse = ","), ")", fex, "|", str_sub(fen, 2, -1), "~", inst_s))
     res <- feols(form, second, cluster = group)
-  } else if (method == "ols") {
+  } else if (method == "ols" | method == "reoi") {
     if (length(fe) == 0) {
       form <- as.Formula(paste0(".[mydep]", fex))
     } else {
@@ -148,6 +140,38 @@ mdqr <- function(formula, data, method = c("fe", "be", "reoi", "regmm", "ols", "
       form <- as.Formula(paste0("c(", paste(mydep, collapse = ","), ")", fex, "|", str_sub(ffe, 2, -1), "|", str_sub(fen, 2, -1), fz))
     }
     res <- feols(form, second, cluster = group)
+  } else   if (method == "be"){
+    b <- cbind(group, end) %>%
+      group_by(group) %>%
+      transmute(across(everything(), funs(mean(.)), .names = "{.col}m"))
+    second <- bind_cols(b[, -1], data, fitted)
+    inst_s <- paste0(paste(endog_var, collapse = "m +"), "m")
+    form <- as.Formula(paste0("c(", paste(mydep, collapse = ","), ")", fex, "|", str_sub(fen, 2, -1), "~", inst_s))
+    res <- feols(form, second, cluster = group)
+  }
+
+  if (method == "reoi"){
+    lambda <- sapply(first, function(x) x[c("lambda_i")])
+
+
+    xlist <- sapply(first, function(x) x[c("mm")])
+    Xtilde <- bdiag(xlist)
+
+
+    # for all U
+    Lambda <- bdiag(lapply(lambda, function(x) x[, , which(u == U)]))
+    lambda <- lapply(first, function(x) x[c("lambda_i")])
+    XVX <- Matrix::tcrossprod(Xtilde %*% Lambda , Xtilde)
+
+
+    residuals <- res[which(u ==U)][[1]]$residuals
+    sig <- 0
+    for (i in 1:G){ #### HERE HELP ME  NEED TO COMPUTE SIGMA! TRY TO COMPY MAYBE PLM() PACKAGE!
+      for (t in 1:data$n){
+        sig <- sig + sum(pooled$residuals[(i*T-T + t)]*pooled$residuals[((i*T-T + (t+1)):(i*T-T +T)  )])
+      }
+    }
+
   }
   return(res)
 }
