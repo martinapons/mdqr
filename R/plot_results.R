c9f35b9953ba4672186d1d2d1b2b4eaee9e84e99
Martina Pons <69034843+martinapons@users.noreply.github.com>
2024-07-10 17:16:29 -0400

    Update plot_results.R change colors
    change colors

--- R/plot_results.R
+++ R/plot_results.R
@@ -11,7 +11,7 @@
 #' For example: \code{plot_results(md_object, "treatment") +  labs(x = "Quantiles", y = "Point Estimates" , title = "My Title")}
 
 
-plot_mdqr <- function(object, x, level = 95){
+plot_mdqr <- function(object, x, level = 95, mycolor = "#0faeb6"){
   q <- 1- (1- 0.01*level)/2
   crit <- stats::qnorm(q)
   tab <- summary_mdqr(object, x)
@@ -22,8 +22,8 @@ plot_mdqr <- function(object, x, level = 95){
     ggplot2::theme_light() +
     ggplot2::theme(legend.position = "bottom") +
     ggplot2::aes(Quantiles, Estimate) +
-    ggplot2::geom_line(size = 1, color = "dodgerblue4") +
-    ggplot2::geom_point(color = "dodgerblue4", size = 2) +
-    ggplot2::geom_ribbon(aes(ymin = lb, ymax = ub), alpha = .2, fill = "deepskyblue2")
+    ggplot2::geom_line(size = 1, color = mycolor) +
+    ggplot2::geom_point(color = mycolor, shape = 20, size = 2) +
+    ggplot2::geom_ribbon(aes(ymin = lb, ymax = ub), alpha = .2, fill = mycolor,color = mycolor, size = .1)
   return(plot)
 }
